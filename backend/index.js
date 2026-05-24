f86af8a799e6269bfc8f1aea17929e4b1624fc78
VINEET KUMAR RAI <vineetrai099@gmail.com>
2026-05-24 23:41:34 +0530

    prepare project for deployment

--- backend/index.js
+++ backend/index.js
@@ -14,13 +14,26 @@ dotenv.config()
 const app = express()
 const _dirname = path.resolve();
 
+// const corsOptions = {
+//   origin: process.env.NODE_ENV === "production"
+//     ? 'https://woman-safety-app.vercel.app'
+//     : ['http://localhost:5173', 'http://127.0.0.1:5173'],
+//   credentials: true,
+//   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+// };
+
+const allowedOrigins = [
+  process.env.FRONTEND_URL,
+  "http://localhost:5173",
+  "http://127.0.0.1:5173",
+];
+
 const corsOptions = {
-  origin: process.env.NODE_ENV === "production"
-    ? 'https://woman-safety-app.vercel.app'
-    : ['http://localhost:5173', 'http://127.0.0.1:5173'],
+  origin: allowedOrigins,
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
 };
+
 
 app.use(cors(corsOptions));
 
